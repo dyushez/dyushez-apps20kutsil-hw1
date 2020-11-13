e9e73731bba6769a3936fff6419c551a9be20f83
dyushez <51192331+dyushez@users.noreply.github.com>
2020-11-13 17:06:23 +0200

    Update TemperatureSeriesAnalysis.java

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -8,8 +8,8 @@ import java.util.Arrays.copyOf;
 public class TemperatureSeriesAnalysis {
 
     public double[] temperatures;
-    public static final double MINIMUM = -273;
-    public static final double EMPTYCELL = -274;
+    public final double MINIMUM = -273;
+    public final double EMPTYCELL = -274;
 
     public TemperatureSeriesAnalysis() {
         temperatures = new double[]{};
